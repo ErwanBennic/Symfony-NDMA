ecaacdddcd75925c2f758cf2a9d0d044db7ccefd
Matthieu <theodore.3103@apside-groupe.com>
2020-05-04 15:15:55 +0200

    Push des familles

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -4,16 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DataController extends AbstractController
 {
+
     /**
-     * @Route("/data", name="data")
+     * @Route("/api/sensor/{name}", name="getSensor", methods={"GET"})
      */
-    public function index()
+    public function getSensorData(string $name)
     {
-        return $this->render('data/index.html.twig', [
-            'controller_name' => 'DataController',
-        ]);
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_OK); 
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode([
+            'value' => 12,
+            'unit' => "température"
+        ]));
+        return $response;
     }
+
+
+    /**
+     * @Route("/api/sensor", name="saveSensor", methods={"PUT"})
+     */
+    public function saveSensorData() 
+    {
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_OK);    
+        $response->prepare();
+        return $response;
+    }
+
 }
