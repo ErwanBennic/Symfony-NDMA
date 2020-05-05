bfe4e872687421b5dc2d229bbd6d14f75c128af0
alexis <alexis.heroin@epsi.fr>
2020-05-05 10:59:31 +0200

    creation commande mqtt

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         // create 20 products! Bam!
         for ($i = 1; $i < 2; $i++) {
             $user = new User();
-            $user->setUsername('admin'.$i);
-            $user->setPassword('admin');
+            $user->setUsername('admin' . $i);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'admin'
+            ));
             $manager->persist($user);
         }
 
