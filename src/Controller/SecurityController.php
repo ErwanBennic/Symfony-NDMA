b1c9d441b36e8028e8f7e27755286b4217a16915
alexis <alexis.heroin@epsi.fr>
2020-05-18 17:29:22 +0200

    add login route

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -27,10 +29,32 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @Route("/api/login", name="api_login", methods={"OPTIONS", "POST"})
+     */
+    public function api_login(Request $request): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        // last username entered by the user
+
+        $response = new Response();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setStatusCode(Response::HTTP_OK);
+
+        $response->setContent(json_encode([$request->request->all()]));
+
+        return $response;
+    }
+
+    /**
      * @Route("/logout", name="app_logout")
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
